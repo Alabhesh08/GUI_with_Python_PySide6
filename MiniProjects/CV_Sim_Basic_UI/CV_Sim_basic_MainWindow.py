be06c653beb2a23ce93736cb2517ffed0df7e031
Labhesh Ajay Sonawane <lasonawane50@gmail.com>
2026-06-11 02:07:45 +0530

    Basic Simulator Layout 1: No Functions

--- MiniProjects/CV_Sim_Basic_UI/CV_Sim_basic_MainWindow.py
+++ MiniProjects/CV_Sim_Basic_UI/CV_Sim_basic_MainWindow.py
@@ -1,6 +1,9 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QMainWindow
 from ui_CV_Sim_basic_MainWindow import Ui_MainWindow
+import numpy as np
+import pyqtgraph as pg
+from PySide6.QtCore import QTimer
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -8,4 +11,71 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         
         self.setupUi(self)
-        # self.setWindowTitle("Basic Simulator Layout")
+        self.setWindowTitle("Basic Simulator Layout")
+
+        # start simulating 3 graphs
+        # Example x-axis
+        self.x = np.linspace(0, 10, 500)
+
+        # Create curves
+        self.pt_curve = self.PT_plot_widget.plot(
+            pen=pg.mkPen('#00E5FF', width=2)
+        )
+
+        self.ft_curve = self.FT_plot_widget.plot(
+            pen=pg.mkPen('#22C55E', width=2)
+        )
+
+        self.pv_curve = self.PV_plot_widget.plot(
+            pen=pg.mkPen('#F59E0B', width=2)
+        )
+
+        # Titles
+        self.PT_plot_widget.setTitle("Pressure vs Time")
+        self.FT_plot_widget.setTitle("Flow vs Time")
+        self.PV_plot_widget.setTitle("Pressure-Volume Loop")
+
+        # Grid
+        for plot in [
+            self.PT_plot_widget,
+            self.FT_plot_widget,
+            self.PV_plot_widget
+        ]:
+            plot.showGrid(x=True, y=True, alpha=0.3)
+
+        self.t = 0
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_plots)
+        self.timer.start(50)
+
+    def update_plots(self):
+
+        self.t += 0.1
+
+        pressure = 120 + 20*np.sin(self.x + self.t)
+
+        flow = 5*np.sin(
+            2*(self.x + self.t)
+        )
+
+        volume = 120 + 40*np.sin(self.x + self.t)
+
+        # Pressure-Time
+        self.pt_curve.setData(
+            self.x,
+            pressure
+        )
+
+        # Flow-Time
+        self.ft_curve.setData(
+            self.x,
+            flow
+        )
+
+        # Pressure-Volume Loop
+        self.pv_curve.setData(
+            volume,
+            pressure
+        )
+        
--- MiniProjects/CV_Sim_Basic_UI/ui_CV_Sim_basic_MainWindow.py
+++ MiniProjects/CV_Sim_Basic_UI/ui_CV_Sim_basic_MainWindow.py
@@ -21,17 +21,37 @@ from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QHBoxLayout
     QMenuBar, QPushButton, QSizePolicy, QStatusBar,
     QVBoxLayout, QWidget)
 
+from pyqtgraph import PlotWidget
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1155, 849)
+        MainWindow.resize(1154, 861)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.layoutWidget = QWidget(self.centralwidget)
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(10, 710, 1131, 91))
+        font = QFont()
+        font.setFamilies([u"Sitka Heading"])
+        font.setBold(True)
+        self.groupBox.setFont(font)
+        self.layoutWidget = QWidget(self.groupBox)
         self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(10, 10, 1131, 691))
-        self.horizontalLayout = QHBoxLayout(self.layoutWidget)
+        self.layoutWidget.setGeometry(QRect(10, 30, 1111, 49))
+        self.verticalLayout_5 = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.Different_Metric_label = QLabel(self.layoutWidget)
+        self.Different_Metric_label.setObjectName(u"Different_Metric_label")
+
+        self.verticalLayout_5.addWidget(self.Different_Metric_label)
+
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(11, 11, 1131, 691))
+        self.horizontalLayout = QHBoxLayout(self.widget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_4 = QVBoxLayout()
@@ -41,51 +61,59 @@ class Ui_MainWindow(object):
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setSizeConstraint(QLayout.SizeConstraint.SetMinimumSize)
-        self.parameter_label = QLabel(self.layoutWidget)
+        self.parameter_label = QLabel(self.widget)
         self.parameter_label.setObjectName(u"parameter_label")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.parameter_label.sizePolicy().hasHeightForWidth())
         self.parameter_label.setSizePolicy(sizePolicy)
-        self.parameter_label.setMinimumSize(QSize(0, 30))
-        font = QFont()
-        font.setFamilies([u"Sitka Heading"])
-        font.setPointSize(10)
-        font.setBold(True)
-        self.parameter_label.setFont(font)
+        self.parameter_label.setMinimumSize(QSize(100, 50))
+        font1 = QFont()
+        font1.setFamilies([u"Sitka Small"])
+        font1.setPointSize(14)
+        font1.setBold(True)
+        self.parameter_label.setFont(font1)
 
         self.verticalLayout.addWidget(self.parameter_label)
 
-        self.hr_label = QLabel(self.layoutWidget)
+        self.hr_label = QLabel(self.widget)
         self.hr_label.setObjectName(u"hr_label")
         sizePolicy.setHeightForWidth(self.hr_label.sizePolicy().hasHeightForWidth())
         self.hr_label.setSizePolicy(sizePolicy)
-        self.hr_label.setFont(font)
+        self.hr_label.setMinimumSize(QSize(100, 0))
+        font2 = QFont()
+        font2.setFamilies([u"Sitka Heading"])
+        font2.setPointSize(10)
+        font2.setBold(True)
+        self.hr_label.setFont(font2)
 
         self.verticalLayout.addWidget(self.hr_label)
 
-        self.resistance_label = QLabel(self.layoutWidget)
+        self.resistance_label = QLabel(self.widget)
         self.resistance_label.setObjectName(u"resistance_label")
         sizePolicy.setHeightForWidth(self.resistance_label.sizePolicy().hasHeightForWidth())
         self.resistance_label.setSizePolicy(sizePolicy)
-        self.resistance_label.setFont(font)
+        self.resistance_label.setMinimumSize(QSize(100, 0))
+        self.resistance_label.setFont(font2)
 
         self.verticalLayout.addWidget(self.resistance_label)
 
-        self.compliance_label = QLabel(self.layoutWidget)
+        self.compliance_label = QLabel(self.widget)
         self.compliance_label.setObjectName(u"compliance_label")
         sizePolicy.setHeightForWidth(self.compliance_label.sizePolicy().hasHeightForWidth())
         self.compliance_label.setSizePolicy(sizePolicy)
-        self.compliance_label.setFont(font)
+        self.compliance_label.setMinimumSize(QSize(100, 0))
+        self.compliance_label.setFont(font2)
 
         self.verticalLayout.addWidget(self.compliance_label)
 
-        self.contractility_label = QLabel(self.layoutWidget)
+        self.contractility_label = QLabel(self.widget)
         self.contractility_label.setObjectName(u"contractility_label")
         sizePolicy.setHeightForWidth(self.contractility_label.sizePolicy().hasHeightForWidth())
         self.contractility_label.setSizePolicy(sizePolicy)
-        self.contractility_label.setFont(font)
+        self.contractility_label.setMinimumSize(QSize(100, 0))
+        self.contractility_label.setFont(font2)
 
         self.verticalLayout.addWidget(self.contractility_label)
 
@@ -95,26 +123,29 @@ class Ui_MainWindow(object):
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setSizeConstraint(QLayout.SizeConstraint.SetMinimumSize)
-        self.start_button = QPushButton(self.layoutWidget)
+        self.start_button = QPushButton(self.widget)
         self.start_button.setObjectName(u"start_button")
         sizePolicy.setHeightForWidth(self.start_button.sizePolicy().hasHeightForWidth())
         self.start_button.setSizePolicy(sizePolicy)
+        self.start_button.setMinimumSize(QSize(200, 0))
         self.start_button.setMaximumSize(QSize(150, 40))
 
         self.gridLayout.addWidget(self.start_button, 0, 0, 1, 1)
 
-        self.pause_button = QPushButton(self.layoutWidget)
+        self.pause_button = QPushButton(self.widget)
         self.pause_button.setObjectName(u"pause_button")
         sizePolicy.setHeightForWidth(self.pause_button.sizePolicy().hasHeightForWidth())
         self.pause_button.setSizePolicy(sizePolicy)
+        self.pause_button.setMinimumSize(QSize(200, 0))
         self.pause_button.setMaximumSize(QSize(150, 40))
 
         self.gridLayout.addWidget(self.pause_button, 1, 0, 1, 1)
 
-        self.reset_button = QPushButton(self.layoutWidget)
+        self.reset_button = QPushButton(self.widget)
         self.reset_button.setObjectName(u"reset_button")
         sizePolicy.setHeightForWidth(self.reset_button.sizePolicy().hasHeightForWidth())
         self.reset_button.setSizePolicy(sizePolicy)
+        self.reset_button.setMinimumSize(QSize(200, 0))
         self.reset_button.setMaximumSize(QSize(150, 40))
 
         self.gridLayout.addWidget(self.reset_button, 2, 0, 1, 1)
@@ -125,61 +156,55 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addLayout(self.verticalLayout_4)
 
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.verticalLayout_2.setSizeConstraint(QLayout.SizeConstraint.SetMinimumSize)
-        self.realtime_plots_label = QLabel(self.layoutWidget)
+        self.verticalLayout_8 = QVBoxLayout()
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.realtime_plots_label = QLabel(self.widget)
         self.realtime_plots_label.setObjectName(u"realtime_plots_label")
         sizePolicy.setHeightForWidth(self.realtime_plots_label.sizePolicy().hasHeightForWidth())
         self.realtime_plots_label.setSizePolicy(sizePolicy)
 
-        self.verticalLayout_2.addWidget(self.realtime_plots_label)
+        self.verticalLayout_8.addWidget(self.realtime_plots_label)
 
-        self.PT_plot_widget = QWidget(self.layoutWidget)
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.PT_plot_widget = PlotWidget(self.widget)
         self.PT_plot_widget.setObjectName(u"PT_plot_widget")
 
         self.verticalLayout_2.addWidget(self.PT_plot_widget)
 
-        self.FT_plot_widget = QWidget(self.layoutWidget)
+
+        self.verticalLayout_8.addLayout(self.verticalLayout_2)
+
+        self.verticalLayout_6 = QVBoxLayout()
+        self.verticalLayout_6.setSpacing(0)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.FT_plot_widget = PlotWidget(self.widget)
         self.FT_plot_widget.setObjectName(u"FT_plot_widget")
 
-        self.verticalLayout_2.addWidget(self.FT_plot_widget)
+        self.verticalLayout_6.addWidget(self.FT_plot_widget)
 
-        self.PV_plot_widget = QWidget(self.layoutWidget)
+
+        self.verticalLayout_8.addLayout(self.verticalLayout_6)
+
+        self.verticalLayout_7 = QVBoxLayout()
+        self.verticalLayout_7.setSpacing(0)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.PV_plot_widget = PlotWidget(self.widget)
         self.PV_plot_widget.setObjectName(u"PV_plot_widget")
 
-        self.verticalLayout_2.addWidget(self.PV_plot_widget)
+        self.verticalLayout_7.addWidget(self.PV_plot_widget)
 
 
-        self.horizontalLayout.addLayout(self.verticalLayout_2)
+        self.verticalLayout_8.addLayout(self.verticalLayout_7)
 
-        self.groupBox = QGroupBox(self.centralwidget)
-        self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(10, 710, 1131, 81))
-        font1 = QFont()
-        font1.setFamilies([u"Sitka Heading"])
-        font1.setBold(True)
-        self.groupBox.setFont(font1)
-        self.widget = QWidget(self.groupBox)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 30, 1111, 49))
-        self.verticalLayout_5 = QVBoxLayout(self.widget)
-        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
-        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
-        self.metrics_panel_label = QLabel(self.widget)
-        self.metrics_panel_label.setObjectName(u"metrics_panel_label")
 
-        self.verticalLayout_5.addWidget(self.metrics_panel_label)
-
-        self.Different_Metric_label = QLabel(self.widget)
-        self.Different_Metric_label.setObjectName(u"Different_Metric_label")
-
-        self.verticalLayout_5.addWidget(self.Different_Metric_label)
+        self.horizontalLayout.addLayout(self.verticalLayout_8)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1155, 25))
+        self.menubar.setGeometry(QRect(0, 0, 1154, 25))
         self.menuDashboard = QMenu(self.menubar)
         self.menuDashboard.setObjectName(u"menuDashboard")
         MainWindow.setMenuBar(self.menubar)
@@ -196,18 +221,17 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Metrics Panel", None))
+        self.Different_Metric_label.setText(QCoreApplication.translate("MainWindow", u"HR       |       CO       |       MAP       |       SV       |       EF       |       LV Pressure       |       Aortic Pressure    ", None))
         self.parameter_label.setText(QCoreApplication.translate("MainWindow", u"Parameters", None))
-        self.hr_label.setText(QCoreApplication.translate("MainWindow", u"HR", None))
-        self.resistance_label.setText(QCoreApplication.translate("MainWindow", u"Resistance", None))
-        self.compliance_label.setText(QCoreApplication.translate("MainWindow", u"Compliance", None))
-        self.contractility_label.setText(QCoreApplication.translate("MainWindow", u"Contractility", None))
+        self.hr_label.setText(QCoreApplication.translate("MainWindow", u"HR :", None))
+        self.resistance_label.setText(QCoreApplication.translate("MainWindow", u"Resistance :", None))
+        self.compliance_label.setText(QCoreApplication.translate("MainWindow", u"Compliance :", None))
+        self.contractility_label.setText(QCoreApplication.translate("MainWindow", u"Contractility :", None))
         self.start_button.setText(QCoreApplication.translate("MainWindow", u"Start", None))
         self.pause_button.setText(QCoreApplication.translate("MainWindow", u"Pause", None))
         self.reset_button.setText(QCoreApplication.translate("MainWindow", u"Reset", None))
         self.realtime_plots_label.setText(QCoreApplication.translate("MainWindow", u"Real-time Plots", None))
-        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Metrics Panel", None))
-        self.metrics_panel_label.setText(QCoreApplication.translate("MainWindow", u"Metrics Panel", None))
-        self.Different_Metric_label.setText(QCoreApplication.translate("MainWindow", u"HR | CO | MAP | SV | EF | LV Pressure | Aortic Pressure    ", None))
         self.menuDashboard.setTitle(QCoreApplication.translate("MainWindow", u"Dashboard", None))
     # retranslateUi
 
